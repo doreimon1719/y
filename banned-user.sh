112c505ea4aed1b68926b1d9a8f16826145526db
Tony Esterdark <messi.klate@gmail.com>
2017-06-08 11:32:50 +0800

    Create banned-user.sh

--- banned-user.sh
+++ banned-user.sh
@@ -10,14 +10,14 @@ fi
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 #MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
 if [ "$MYIP" = "" ]; then
-	MYIP=$(wget -qO- 139.59.121.46)
+	MYIP=$(wget -qO- ipv4.icanhazip.com)
 fi
-#MYIP=$(wget -qO- 139.59.121.46)
+#MYIP=$(wget -qO- ipv4.icanhazip.com)
 
 
 echo "------- ---------- BANNED AKUN SSH --------------------"
 
-	echo "         DEVELOPED BY HuHu            "
+	echo "         DEVELOPED BY huhu            "
 echo ""
 
 # begin of user-list
